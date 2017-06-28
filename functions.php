d37a277722babbbc3cdf2f20ca05466aa4639129
neoFelhz <neoFelhz@gmail.com>
2017-06-28 19:03:04 +0800

    Add compress html support

--- functions.php
+++ functions.php
@@ -267,3 +267,54 @@ function is_pjax()
 {
     return array_key_exists('HTTP_X_PJAX', $_SERVER) && $_SERVER['HTTP_X_PJAX'];
 }
+
+//Compress the code
+//Using <!--<nocompress>--><!--</nocompress>--> for compatiblity
+function compressHtml($html_source) {
+    $chunks = preg_split('/(<!--<nocompress>-->.*?<!--<\/nocompress>-->|<nocompress>.*?<\/nocompress>|<pre.*?\/pre>|<textarea.*?\/textarea>|<script.*?\/script>)/msi', $html_source, -1, PREG_SPLIT_DELIM_CAPTURE);
+    $compress = '';
+    foreach ($chunks as $c) {
+        if (strtolower(substr($c, 0, 19)) == '<!--<nocompress>-->') {
+            $c = substr($c, 19, strlen($c) - 19 - 20);
+            $compress .= $c;
+            continue;
+        } else if (strtolower(substr($c, 0, 12)) == '<nocompress>') {
+            $c = substr($c, 12, strlen($c) - 12 - 13);
+            $compress .= $c;
+            continue;
+        } else if (strtolower(substr($c, 0, 4)) == '<pre' || strtolower(substr($c, 0, 9)) == '<textarea') {
+            $compress .= $c;
+            continue;
+        } else if (strtolower(substr($c, 0, 7)) == '<script' && strpos($c, '//') != false && (strpos($c, "\r") !== false || strpos($c, "\n") !== false)) {
+            $tmps = preg_split('/(\r|\n)/ms', $c, -1, PREG_SPLIT_NO_EMPTY);
+            $c = '';
+            foreach ($tmps as $tmp) {
+                if (strpos($tmp, '//') !== false) {
+                    if (substr(trim($tmp), 0, 2) == '//') {
+                        continue;
+                    }
+                    $chars = preg_split('//', $tmp, -1, PREG_SPLIT_NO_EMPTY);
+                    $is_quot = $is_apos = false;
+                    foreach ($chars as $key => $char) {
+                        if ($char == '"' && $chars[$key - 1] != '\\' && !$is_apos) {
+                            $is_quot = !$is_quot;
+                        } else if ($char == '\'' && $chars[$key - 1] != '\\' && !$is_quot) {
+                            $is_apos = !$is_apos;
+                        } else if ($char == '/' && $chars[$key + 1] == '/' && !$is_quot && !$is_apos) {
+                            $tmp = substr($tmp, 0, $key);
+                            break;
+                        }
+                    }
+                }
+                $c .= $tmp;
+            }
+        }
+        $c = preg_replace('/[\\n\\r\\t]+/', ' ', $c);
+        $c = preg_replace('/\\s{2,}/', ' ', $c);
+        $c = preg_replace('/>\\s</', '> <', $c);
+        $c = preg_replace('/\\/\\*.*?\\*\\//i', '', $c);
+        $c = preg_replace('/<!--[^!]*-->/', '', $c);
+        $compress .= $c;
+    }
+    return $compress;
+}
